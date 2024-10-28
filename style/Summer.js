6165605cad77c788e8b8cd2eb9dbcae96e55b9af
ninnin4901 <Vinhduong04092001@gmail.com>
2024-10-28 08:46:38 +0700

    upload page 2

---
+++ style/Summer.js
@@ -0,0 +1,17 @@
+function myFunction2() {
+  var dropdown = document.getElementById("myDropdown2");
+  dropdown.classList.toggle("show"); // Thêm hoặc xóa class "show"
+}
+
+// Đóng dropdown nếu người dùng nhấp ra ngoài
+window.onclick = function(event) {
+  if (!event.target.closest('.language-toggle')) { // Kiểm tra nếu nhấp ngoài phần tử toggle
+      var dropdowns = document.getElementsByClassName("dropdown_content");
+      for (var i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains("show")) {
+              openDropdown.classList.remove("show"); // Đóng dropdown
+          }
+      }
+  }
+};
